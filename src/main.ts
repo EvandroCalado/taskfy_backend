e62b789012a5cc1a27c7d9ae1b0f8d6ded3f5624
Evandro Calado <evandrocalado07@gmail.com>
2025-06-09 09:01:06 -0300

    feat: Setup configuration module and cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+
+  const config = app.get(ConfigService);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+  app.enableCors({
+    origin: config.getOrThrow<string>('APPLICATION_ALLOWED_ORIGINS'),
+    credentials: true,
+    exposedHeaders: ['Set-Cookie'],
+  });
+
+  await app.listen(config.getOrThrow<number>('APPLICATION_PORT'));
 }
 void bootstrap();
